b2e3594199ac67ca98ca73f820679b88e3a62b67
BT <bahartofigh@gmail.com>
2023-09-04 19:32:53 +0430

    test App.js module

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,23 @@
+// import CategoryView from "./CategoryView.js";
+// import ProductView from "./ProductView.js";
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   // setApp => categories : OK
+  // CategoryView.setApp();
+  // ProductView.setApp();
+  // create categories options
+//   CategoryView.createCategoriesList();
+  
+//   ProductView.createProductsList(ProductView.products);
+// });
+
 import CategoryView from "./CategoryView.js";
 import ProductView from "./ProductView.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // setApp => categories : OK
   CategoryView.setApp();
   ProductView.setApp();
-  // create categories options
+
   CategoryView.createCategoriesList();
-  
-  ProductView.createProductsList(ProductView.products);
+  ProductView.createProductsList();
 });
